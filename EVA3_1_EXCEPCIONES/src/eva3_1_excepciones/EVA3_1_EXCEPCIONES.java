c2044a27c54b5be23fc40b6b311e2e30be5b49b6
faze-useless <143573781+faze-useless@users.noreply.github.com>
2024-04-22 18:50:00 -0600

    Add files via upload

---
+++ EVA3_1_EXCEPCIONES/src/eva3_1_excepciones/EVA3_1_EXCEPCIONES.java
@@ -0,0 +1,22 @@
+package eva3_1_excepciones;
+
+import java.util.*;
+/**
+ *
+ * @author DELL
+ */
+public class EVA3_1_EXCEPCIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Introduzca un núumero:");
+        int num = input.nextInt();
+        System.out.println("El número fue "+num);
+    }
+    
+}
